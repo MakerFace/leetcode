22a3a0e1dec5c1ed7a7b2b24c2451220d331a029
MakerFace <chenqi@imudges.com>
2022-04-28 14:03:51 +0800

    add cmake build type

---
+++ src/zyk.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file zyk.cpp
+ * @brief
+ * @author chenqi (chenqi@imudges.com)
+ * @version 1.0
+ * @date 2022-04-28
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+bool compare(int x, int y)
+{
+    return x <= y;
+}
+int main()
+{
+    vector<int> nums = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+    auto compare = [](int x, int y)
+    {
+        return (x < y);
+    };
+    // sort(nums.begin(), nums.end(), compare);
+    sort(nums.begin(), nums.end(), compare);
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        std::cout << nums[i] << " ";
+    }
+    std::cout << std::endl;
+}
