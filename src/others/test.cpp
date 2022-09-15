1c7eae0b0836146f94b0c864037425afb2f1d68f
MakerFace <chenqi@imudges.com>
2022-09-15 21:39:57 +0800

    constrainting template

--- src/others/test.cpp
+++ src/others/test.cpp
@@ -67,18 +67,49 @@ class Base {
 
 class Test : public Base {
  private:
-  int x;
-  int y;
+  // int x;
+  // int y;
 
  public:
   Test() {}
-  Test(int _x) : x(10), y(x) {}
+  Test(int _x) {}
   explicit operator int() { return 1; }
   virtual int get(int x = 3) {
     return x;
   }  // 默认参数无效，使用的仍是父类的默认参数（缺省参数静态绑定导致）
 };
 __attribute__((constructor)) void before() { printf("before main\n"); }
+
+class Person {
+ public:
+  template <typename T,
+            typename = typename std::enable_if<
+                !std::is_base_of<Person, typename std::decay<T>::type>::value &&
+                !std::is_integral<
+                    typename std::remove_reference<T>::type>::value>::type>
+  explicit Person(T &&n) : name(std::forward<T>(n)) {
+    std::cout << "forward" << std::endl;
+  }
+
+  explicit Person() { std::cout << "null" << std::endl; }
+
+  explicit Person(int idx) { std::cout << "int" << std::endl; }
+
+  explicit Person(const Person &rhs) : name(rhs.name) {
+    std::cout << "const Person&" << std::endl;
+  }
+
+  explicit Person(Person &&rhs) : name(std::forward<std::string>(rhs.name)) {
+    std::cout << "Person&&" << std::endl;
+  }
+
+ private:
+  std::string name;
+};
+Person p1;
+Person p2("string");
+Person p3(16);
+Person p4(p2);  // 复制构造函数
 
 int before(int x) {
   printf("before main %d\n", x);
