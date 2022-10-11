b86b3ae4dba8e7a8fda58c62281dd97cec6598ba
MakerFace <chenqi@imudges.com>
2022-10-11 10:22:51 +0800

    10.10

--- src/Oct/10.10/785.cpp
+++ src/Oct/10.10/785.cpp
@@ -11,45 +11,62 @@
 using namespace std;
 
 class Solution {
+  enum class Color { UNCOLORED, RED, GREEN };
+
  public:
   bool isBipartite(vector<vector<int>>& graph) {
     int n = static_cast<int>(graph.size());
-    vector<set<int>> graph_set(n);
-    sets[0].clear();
-    sets[1].clear();
-    used.clear();
-    used.resize(n);
+    color.resize(n);
+    valid = true;
     for (int i = 0; i < n; ++i) {
-      for (auto& n : graph[i]) {
-        graph_set[i].insert(n);
+      if (color[i] == Color::UNCOLORED) bfs(graph, Color::RED, i);
+      if (not valid) break;
+    }
+    return valid;
+  }
+
+ private:
+  void bfs(vector<vector<int>>& graph, Color c, int node) {
+    queue<int> que;
+    que.push(node);
+    color[node] = c;
+    int top;
+    while (not que.empty()) {
+      top = que.front();
+      que.pop();
+      c = color[top] == Color::RED ? Color::GREEN : Color::RED;
+      for (auto nei : graph[top]) {
+        if (color[nei] == Color::UNCOLORED) {
+          color[nei] = c;
+          que.push(nei);
+        } else if (color[nei] != c) {
+          valid = false;
+          return;
+        }
       }
     }
+  }
 
-    bool ans = false;
-    for (int i = 0; i < n; ++i) {
-      if (not used[i] and dfs(graph_set, 0, i)) ans = true;
-      ans &= used[i];
+  /**
+   * @brief 相邻节点染色法，如果重复节点染色不一致，就非法，一致跳过
+   */
+  void dfs(vector<vector<int>>& graph, Color c, int node) {
+    color[node] = c;
+    c = c == Color::RED ? Color::GREEN : Color::RED;
+    for (auto neighbor : graph[node]) {
+      if (color[neighbor] == Color::UNCOLORED) {
+        dfs(graph, c, neighbor);
+        if (not valid) return;
+      } else if (color[neighbor] != c) {
+        valid = false;
+        return;
+      }
     }
-    return ans;
   }
 
  private:
-  bool dfs(vector<set<int>>& graph, int sidx, int node) {
-    used[node] = 1;
-    for (auto n : sets[sidx]) {
-      if (graph[n].find(node) != graph[n].end()) return false;
-    }
-    sets[sidx].insert(node);
-    for (auto& n : graph[node]) {
-      if (not used[n] and dfs(graph, not sidx, n)) return true;
-    }
-
-    return sets[0].size() + sets[1].size() == graph.size();
-  }
-
- private:
-  set<int> sets[2];
-  vector<int> used;
+  vector<Color> color;
+  bool valid;
 };
 
 constexpr int len() { return 5; }
