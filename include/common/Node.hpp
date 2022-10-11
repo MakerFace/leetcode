3ba64a828d5df5a2073c0bfe4ceddf5c910a8615
MakerFace <chenqi@imudges.com>
2022-10-11 19:45:50 +0800

    add namespace tree

--- include/common/Node.hpp
+++ include/common/Node.hpp
@@ -1,63 +1,53 @@
 #ifndef NODE_HPP
 #define NODE_HPP
 
-#include <vector>
-#include <queue>
 #include <initializer_list>
+#include <queue>
+#include <vector>
 #define null -1
 
+namespace tree {
+class Node {
+ public:
+  int val;
+  std::vector<Node *> children;
 
-class Node
-{
-public:
-    int val;
-    std::vector<Node *> children;
+  Node() {}
 
-    Node() {}
+  Node(int _val) { val = _val; }
 
-    Node(int _val)
-    {
-        val = _val;
-    }
-
-    Node(int _val, std::vector<Node *> _children)
-    {
-        val = _val;
-        children = _children;
-    }
+  Node(int _val, std::vector<Node *> _children) {
+    val = _val;
+    children = _children;
+  }
 };
 
-Node *list2Tree(std::initializer_list<int> l)
-{
-    std::queue<Node *> queue;
-    Node *head = nullptr;
-    for (auto iter = l.begin(); iter != l.end();)
-    {
-        if (queue.empty())
-        {
-            head = new Node(*iter);
-            queue.push(head);
-            ++iter;
-        }
-        else
-        {
-            std::vector<Node *> children;
-            while ((iter != l.end()) && (*iter != -1))
-            {
-                Node *temp = new Node(*iter);
-                children.emplace_back(temp);
-                queue.push(temp);
-                ++iter;
-            }
-            queue.front()->children.swap(children);
-            queue.pop();
-        }
-        if (iter != l.end())
-        {
-            ++iter;
-        }
+}  // namespace tree
+
+tree::Node *list2Tree(std::initializer_list<int> l) {
+  std::queue<tree::Node *> queue;
+  tree::Node *head = nullptr;
+  for (auto iter = l.begin(); iter != l.end();) {
+    if (queue.empty()) {
+      head = new tree::Node(*iter);
+      queue.push(head);
+      ++iter;
+    } else {
+      std::vector<tree::Node *> children;
+      while ((iter != l.end()) && (*iter != -1)) {
+        tree::Node *temp = new tree::Node(*iter);
+        children.emplace_back(temp);
+        queue.push(temp);
+        ++iter;
+      }
+      queue.front()->children.swap(children);
+      queue.pop();
     }
-    return head;
+    if (iter != l.end()) {
+      ++iter;
+    }
+  }
+  return head;
 }
 
-#endif // NODE_HPP
+#endif  // NODE_HPP
