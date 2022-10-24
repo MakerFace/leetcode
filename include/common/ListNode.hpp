57d2dfd1fb19eec10feb63ce1e5f90892c2baf8d
MakerFace <chenqi@imudges.com>
2022-10-24 21:48:29 +0800

    add Vector.hpp to ListNode.hpp

--- include/common/ListNode.hpp
+++ include/common/ListNode.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include "common/Vector.hpp"
 
 struct ListNode {
   int val;
@@ -67,8 +68,8 @@ void print_list(const ListNode *root, std::ostream &out = std::cout) {
   out << "]" << std::endl;
 }
 
-std::ostream &operator<<(std::ostream &out, const ListNode *root) {
-  print_list(root, out);
+std::ostream &operator<<(std::ostream &out, const ListNode root) {
+  print_list(&root, out);
   return out;
 }
 
